a0d66dc03423c2d9684eb46fd1338f5b5aca1819
beda.bora <bedabor1997@gmail.com>
2023-09-13 06:38:45 +0530

    Working with user input data in request response

--- Day-2/app.js
+++ Day-2/app.js
@@ -8,13 +8,42 @@
 
 // import http from "http";
 const http = require("http");
+const fs = require("fs");
 
 const reqListener = (request, response) => {
-  console.log(request.url, request.method, request.headers);
+  const url = request.url;
+  const method = request.method;
+  if (url === "/") {
+    response.write("<html>");
+    response.write("<head><title>Enter Message</title></head>");
+    response.write("<body>");
+    response.write(
+      "<form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form>"
+    );
+    response.write("</body>");
+    response.write("</html>");
+    return response.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    request.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      fs.writeFileSync("message.txt", parsedBody.split("=")[1]);
+    });
+    response.statusCode = 302;
+    response.setHeader("Location", "/");
+    return response.end();
+  }
   response.setHeader("Content-Type", "text/html");
-  response.write(
-    "<html><head><title>Hello</title></head><body>Helllo World</body></html>"
-  );
+  response.write("<html>");
+  response.write("<head><title>Hello</title></head>");
+  response.write("<body>");
+  response.write("Hello World");
+  response.write("</body>");
+  response.write("</html>");
   response.end();
 };
 
