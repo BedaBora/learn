1d137b233c8f2b83c6bc33000f4089ffba6a7854
beda.bora <bedabor1997@gmail.com>
2023-09-26 21:06:38 +0530

    added static file routing for css

--- Day-4/app.js
+++ Day-4/app.js
@@ -38,6 +38,7 @@ const shopRoutes = require("./routes/shop");
 
 /*****************************/
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(adminRoutes);
 
