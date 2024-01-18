96f0e96611c7f0ca8625c72659cfc2006e46abef
beda.bora <bedabor1997@gmail.com>
2024-01-18 15:24:50 +0530

    conditions

--- Fundamentals/Part-1/script.js
+++ Fundamentals/Part-1/script.js
@@ -165,7 +165,7 @@ string`); // Multiline string after ES6
 */
 
 // IF-ELSE Condition
-/*
+
 const age = 17;
 const isOldEnough = age >= 18;
 if (isOldEnough) {
@@ -184,13 +184,3 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
-*/
-
-// CHALLENGE #2
-/*
-Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
-1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
-"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
-2. Modify the outputs above to use template literals to include the BMI values in the outputs.
-Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
-*/
