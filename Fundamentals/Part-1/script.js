f62cefa7ec82c75c797da7f371e3f8ed9d4efc39
beda.bora <bedabor1997@gmail.com>
2024-01-18 10:42:43 +0530

    values and variables

--- Fundamentals/Part-1/script.js
+++ Fundamentals/Part-1/script.js
@@ -1,4 +1,17 @@
-let js = "amazing";
-if (js === "amazing") alert("JS is Fun");
+// let js = "amazing";
+// console.log(40 + 8 + 20);
 
-console.log(40 + 8 + 20);
+/* 
+    A Value is just a piece of data. Eg. "Jonas"
+    We can store values in a variable to use the value over and over again
+*/
+
+let firstName = "Jonas"; // Store the value Jonas in memory
+// in JS, we use camelCasing convention to name variables
+// Variable names cannot start with a number or any special character other than _
+// Variable names cannot contain any special character other than _
+// Reserved keywords cannot be used as variables names.
+// Vaiables with all UPPER CASE are used as constants.
+// Try to give meaningful names to the variables. For Eg. use firstJob instead of job1 as variable name
+
+console.log(firstName); // console.log() is used to write to console.
