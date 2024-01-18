ae1e798580845f458e5110ba62755de0a4db6132
beda.bora <bedabor1997@gmail.com>
2024-01-18 15:23:36 +0530

    Revert "Challenge #3"
    This reverts commit 1154a9558d70c15f42e0a3653906cb01ae460077.

--- Fundamentals/Part-1/script.js
+++ Fundamentals/Part-1/script.js
@@ -194,21 +194,3 @@ Use the BMI example from Challenge #1, and the code you already wrote, and impro
 2. Modify the outputs above to use template literals to include the BMI values in the outputs.
 Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
 */
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-
-/* Write your code below. Good luck! 🙂 */
-
-let message;
-if (BMIMark > BMIJohn) {
-  message = `Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`;
-} else {
-  message = `John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`;
-}
-console.log(message);
