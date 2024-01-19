c06141de58a6a967941f57758841c671803dba6c
beda.bora <bedabor1997@gmail.com>
2024-01-19 11:11:00 +0530

    functions

--- Fundamentals/Part-2/script.js
+++ Fundamentals/Part-2/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+/*
+    A function is a piece of code that we can use over and over again.
+
+*/
+
+// defining the function
+function logger(message) {
+  console.log(message);
+}
+
+// calling/running/invoking the function
+logger("Hello");
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice; // this function returns a value
+}
+
+logger(fruitProcessor(5, 2));
