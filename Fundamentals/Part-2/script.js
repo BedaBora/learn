a28a032a7058b6d5fc5ffdcab14de80f55a3fac3
beda.bora <bedabor1997@gmail.com>
2024-01-19 12:27:54 +0530

    more on functions

--- Fundamentals/Part-2/script.js
+++ Fundamentals/Part-2/script.js
@@ -4,7 +4,7 @@
     A function is a piece of code that we can use over and over again.
 
 */
-
+/*
 // defining the function
 function logger(message) {
   console.log(message);
@@ -20,3 +20,53 @@ function fruitProcessor(apples, oranges) {
 }
 
 logger(fruitProcessor(5, 2));
+*/
+
+//FUNCTION DECLARATION VS EXPRESSION
+
+/*
+    main difference between funtion declaration and expression is
+    in function declaration, the function can be called before the function is declared
+    but in a function expression, we cannot call it until we define it
+    this happends because of hoisting.
+    preferably use function expression
+*/
+//declaration
+function calcAge(birthYear) {
+  return 2037 - birthYear;
+}
+
+console.log(calcAge(1997));
+
+//expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+console.log(calcAge2(1990));
+
+// ARROW FUNCTIONS - a special form of function expression
+const calcAge3 = (birthYear) => 2023 - birthYear;
+console.log(calcAge3(1658));
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Beda"));
+
+// FUNCTIONS CALLING OTHER FUNTIONS
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`;
+  return juice; // this function returns a value
+}
+
+console.log(fruitProcessor(2, 3));
