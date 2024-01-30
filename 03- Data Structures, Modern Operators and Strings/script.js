d50613b931283367fb4e09634cc60acef9372a92
beda.bora <bedabor1997@gmail.com>
2024-01-30 17:14:36 +0530

    optional chaining

--- 03- Data Structures, Modern Operators and Strings/script.js
+++ 03- Data Structures, Modern Operators and Strings/script.js
@@ -15,7 +15,7 @@ const openingHours = {
     open: 11,
     close: 23,
   },
-  [`day-${2 + 4}`]: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -401,4 +401,26 @@ for (const [i, el] of menu.entries()) {
 }
 */
 
-// OBJECT LITERALS
+// OPTIONAL CHAINING
+
+//Without optional chaining
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// With optional chaining
+// optional chaining operator ?. the field after . is computed only if the field before ? is not undefined
+console.log(restaurant.openingHours?.mon?.open); // property mon will be excessed onli if opening hour exists
+
+//Example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+  console.log(restaurant.openingHours[day]?.open);
+}
+
+console.log(restaurant.order?.(0, 1) ?? "Method dont exist");
+console.log(restaurant.orderResotto?.(0, 1) ?? "Method dont exist");
+
+const users = [{ name: "Beda", email: "beda@gmail.com" }];
+
+console.log(users[0]?.name ?? "User empty");
