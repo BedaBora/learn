66e37fcc31957fb34d25d4bb3e3481ab0130f703
beda.bora <bedabor1997@gmail.com>
2024-02-08 15:16:11 +0530

    Challenge #3

--- 03- Data Structures, Modern Operators and Strings/script.js
+++ 03- Data Structures, Modern Operators and Strings/script.js
@@ -574,6 +574,7 @@ console.log(orderesSet[0]); // will not work since in a set, there are not indic
 // map is a ds that is used to store data in key value form
 // in object, keys are always strings. but in maps, key can be any datatype
 
+/*
 const rest = new Map();
 rest.set("name", "classico Italliano");
 rest.set(1, "Firenze, Italy");
@@ -631,3 +632,92 @@ for (const [key, value] of question) {
 
 //convert map to array
 console.log(...question);
+*/
+
+// Sources of data
+// 1. From the program itself. i.e data written directly into source code
+// 2. From ui. i.e data stored inside the DOM
+// 3. from enternal sources. i.e data fetched from apis
+
+// Deciding which ds to use to store data
+/*
+              |
+        |           |
+    Simple list   Key-value
+        |           |
+    array or set  map or objects
+
+
+Arrays
+- Use when we need ordered list of values
+- when we need data manipulation
+
+Sets
+- Use when we need to work with unique vlaues
+- use when high-performance is really important
+- use to remove duplicates from array
+
+Objects
+- traditional key-value store
+- easier to write and access values
+- use when we need to include methods
+- used when working with json
+
+Maps
+- better performance
+- keys can ahve any data type
+- easy to iterate and compute size
+- use when we simply need to map key and values
+- use when keys are not strings
+*/
+
+//------------------ CHALLENGE #2 ------------------//
+/*
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+*/
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2
+gameEvents.delete(64);
+
+// 3
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4
+for (const [key, val] of gameEvents.entries()) {
+  console.log(
+    `[${(key <= 45 && "FIRST HALF") || "SECOND HALF"}] ${key}: ${val}`
+  );
+}
