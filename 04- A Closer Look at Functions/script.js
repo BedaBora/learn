5bab071743fc4864a897b4514f70ab3f91f5027a
beda.bora <bedabor1997@gmail.com>
2024-02-26 09:17:39 +0530

    closure

--- 04- A Closer Look at Functions/script.js
+++ 04- A Closer Look at Functions/script.js
@@ -274,6 +274,7 @@ poll.displayResults.call({ answers: data1 });
 poll.displayResults.call({ answers: data2 }, "string");
 */
 
+/*
 // we can run this fucntion multiple times
 // const runOnce = function () {
 //   console.log("Run this only once");
@@ -286,3 +287,58 @@ poll.displayResults.call({ answers: data2 }, "string");
 (function () {
   console.log("Run this only once");
 })();
+*/
+
+//Closure
+// closure is not an explicit functionality of js functions
+const secureBoking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBoking();
+booker();
+booker();
+booker();
+
+//the booker function is able to access the passengerCount and update it because of closure
+//closure makes a function remember all the variables initiated at the function birthpalce
+// any function has access to the variable environment of the execution context it was called in
+// this conneection is called closure
+// closure is the variable environment exactly as it was at the time and place the function was created
+// closure has high priority in the scope chain
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+h();
+f();
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds.`);
+};
+
+boardPassengers(100, 3);
