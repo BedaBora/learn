0a3620064bbc55de0f88e5f1731d2f038e918af7
beda.bora <bedabor1997@gmail.com>
2024-09-09 19:24:50 +0530

    Day_1

---
+++ Day01/task.py
@@ -0,0 +1,23 @@
+"""
+#PRINTING
+print("Hello World")
+
+#STRING MANIPULATION
+print("Hello world!\nHello world!\nHello world!")
+print("Hello"+" "+"Beda")
+
+#INPUTS
+my_name=input("Enter your name: ")
+print("My name is" + " " + my_name+"!")
+
+#VARIABLES
+username=input("What is your name? ")
+length=len(username)
+print(username, length)
+"""
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+band_name = city+" "+pet
+print("Your band name could be "+band_name)
